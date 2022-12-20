738db9cb1d63ba0a56a41807b0dc8178f33de1d6
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2022-12-20 23:01:43 +0100

    Worked with react props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Jokes from './Jokes';
+export default function App() {
+ return(
+    <div>
+        <Jokes 
+           Setup="I got my daughter a fridge for her birthday"
+           Punchline="I can't wait to see her face light up when she opens it."   
+        />
+        <Jokes
+           Setup="How did hacker escape the police?"
+           Punchline="He just ran somware."
+        />
+         <Jokes
+           Setup="Why do bee stay in the hive in the winter?"
+           Punchline="Swarm."
+        />
+         <Jokes
+          Setup="Why dont pirates travel on a mountain road?"
+          Punchline="Scurvy."
+        />
     </div>
-  );
+ )
 }
 
-export default App;
---
+++ src/Jokes.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Jokes({Setup, Punchline}){
+    return(
+        <div>
+      <h1>
+        setup= {Setup} <br></br>
+       
+        </h1>
+         Punchline = {Punchline}
+         <hr />
+         </div>
+    )
+}
